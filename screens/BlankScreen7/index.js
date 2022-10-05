f67546436e8a54cfbdd04de8f2e0649c73a669d5
Rodrigo <rodrigo@crowdbotics.com>
2022-10-05 22:46:12 +0000

    Add Blank Screen 7

---
+++ screens/BlankScreen7/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen7 = () => {
+  return <View style={_styles.BlankScreen7}>
+      </View>;
+};
+
+export default BlankScreen7;
+
+const _styles = StyleSheet.create({
+  BlankScreen7: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
