9f46df48f1d2ad085c1aab98c12ac78bb942ff35
Rodrigo <rodrigo@crowdbotics.com>
2022-10-05 22:40:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "blank_screens_2_21",
+  url: "https://blank_screens_2_21.botics.co",
+  api: "https://blank_screens_2_21.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
