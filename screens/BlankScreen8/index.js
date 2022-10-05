ba67bfa25ff8b25b2b5a9a4f12170983c965fedf
Rodrigo <rodrigo@crowdbotics.com>
2022-10-05 22:46:17 +0000

    Add Blank Screen 8

---
+++ screens/BlankScreen8/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen8 = () => {
+  return <View style={_styles.BlankScreen8}>
+      </View>;
+};
+
+export default BlankScreen8;
+
+const _styles = StyleSheet.create({
+  BlankScreen8: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
