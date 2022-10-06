e84207a59b5db3e54e862b0974f0f4de2035f74c
Rodrigo <rodrigo@crowdbotics.com>
2022-10-06 20:17:31 +0000

    Add Blank Screen 11

---
+++ screens/BlankScreen11/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen11 = () => {
+  return <View style={_styles.BlankScreen11}>
+      </View>;
+};
+
+export default BlankScreen11;
+
+const _styles = StyleSheet.create({
+  BlankScreen11: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
